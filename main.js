6face108279542dfb885ae250ef130810fc20f5b
Alem0nes <rhernandes91@gmail.com>
2024-09-13 06:48:48 +0100

    add_fetch_catch_finally

--- main.js
+++ main.js
@@ -10,26 +10,59 @@ $(document).ready(function(){
         const gitFallowing = document.querySelector("#gitFallowing")
         const gitLink = document.querySelector("#gitLink")
         const gitAvatar = document.querySelector("#gitAvatar")
+        
+        //fetch catch finally
 
-        $.ajax(endPoint).done(function(resposta){
-            const avatar = resposta.avatar_url
-            const user = resposta.login
-            const name = resposta.name;
-            const tarefas = resposta.public_repos;
-            const fallowers = resposta.followers;
-            const fallowing = resposta.following;
-            const link = resposta.html_url;
-
-            gitAvatar.src = avatar;
-            gitUser.innerHTML = user;
-            gitName.innerHTML = name;
-            gitTarefas.innerHTML = tarefas;
-            gitFallowers.innerHTML = fallowers
-            gitFallowing.innerHTML = fallowing
-            gitLink.href = link;
-
-
+        fetch(endPoint).then(function(response){
+            if(!response.ok) {
+                throw new Error ('Try again later!')
+            } return response.json();
         })
+            .then(function(resposta){
+                    
+                const avatar = resposta.avatar_url
+                const user = resposta.login
+                const name = resposta.name;
+                const tarefas = resposta.public_repos;
+                const fallowers = resposta.followers;
+                const fallowing = resposta.following;
+                const link = resposta.html_url;
+        
+                gitAvatar.src = avatar;
+                gitUser.innerHTML = user;
+                gitName.innerHTML = name;
+                gitTarefas.innerHTML = tarefas;
+                gitFallowers.innerHTML = fallowers
+                gitFallowing.innerHTML = fallowing
+                gitLink.href = link;
+        })
+        .catch(function(error){
+            alert('Teve algum problema!', error)
+        })
+        .finally(function(){
+            $('#gituserempty').val('')
+        })
+
+        //Ajax
+
+        // $.ajax(endPoint).done(function(resposta){
+        //     const avatar = resposta.avatar_url
+        //     const user = resposta.login
+        //     const name = resposta.name;
+        //     const tarefas = resposta.public_repos;
+        //     const fallowers = resposta.followers;
+        //     const fallowing = resposta.following;
+        //     const link = resposta.html_url;
+
+        //     gitAvatar.src = avatar;
+        //     gitUser.innerHTML = user;
+        //     gitName.innerHTML = name;
+        //     gitTarefas.innerHTML = tarefas;
+        //     gitFallowers.innerHTML = fallowers
+        //     gitFallowing.innerHTML = fallowing
+        //     gitLink.href = link;
+        // })
+
     // Json
 
     // const gitName = document.querySelector("#gitName")
